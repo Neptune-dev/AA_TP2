29f9048a805448912f0ef2b9181a93ae9323723c
Neptune <felix@ladislas.fr>
2026-03-21 11:12:43 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -5,8 +5,7 @@
 int main (int argc, char * argv[])
 {
     int test[] = {1, -8, 4, -5, -5, 3, 2, -1, 3, 0};
-    m3 (test, 10);
-    //Seq res = m2 (test, 9);
-    //res.print(&res);
+    Seq res = m2 (test, 9);
+    res.print(&res);
     return 0;
 }
