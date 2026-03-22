6ddd4fdb3017b873d4a4932ffbc3d7ee2e626532
Neptune <felix@ladislas.fr>
2026-03-22 11:24:06 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -5,29 +5,14 @@
 
 int main (int argc, char * argv[])
 {
-    //int maxTableSize = 1000;
-    //int var = 10;
-//
-    //double** tbl = testRoutine (maxTableSize, var, 3, m1, m2, m3);
-    ////printResults(tbl, maxTableSize, 3);
-    //exportResultsToCSV(tbl, maxTableSize, 3, "test.csv");
-//
-    //freeResults(tbl, 3);
+    int maxTableSize = 1000;
+    int var = 10;
 
+    double** tbl = testRoutine (maxTableSize, var, 3, m1, m2, m3);
+    //printResults(tbl, maxTableSize, 3);
+    exportResultsToCSV(tbl, maxTableSize, 3, "test.csv");
 
-    int tbl[100];
-    
-    initRandom();
-    randomiseTable(tbl, 100, 10);
-    printTable(tbl, 100, "tbl");
-
-    Seq s1 = m1(tbl, 100);
-    Seq s2 = m2(tbl, 100);
-    Seq s3 = m3(tbl, 100);
-
-    s1.print(&s1);
-    s2.print(&s2);
-    s3.print(&s3);
+    freeResults(tbl, 3);
 
     return 0;
 
